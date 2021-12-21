2163296e7c5de9f729f7a9e8bc8d017aaa9447e5
Asako Kondo <kondoasako@gmail.com>
2021-12-21 17:54:26 +0900

    トップページを作成

---
+++ src/components/Board.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <header>
+            my Trello
+        </header>
+        <main>
+            <p class="info-line">All: 0 Tasks</p>
+        </main>
+    </div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
