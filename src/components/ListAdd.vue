67a6a558083b424636eb2162db0e115bdc12490f
Asako Kondo <kondoasako@gmail.com>
2021-12-21 19:45:59 +0900

    フォーカスされたときのスタイル

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,9 +1,11 @@
 <template>
-    <form class="addlist" @submit.prevent="addList">
+    <form :class="classList" @submit.prevent="addList">
         <input v-model="title"
                 type="text"
                 class="text-input"
-                placeholder="リストを追加">
+                placeholder="リストを追加"
+                @focusin="startEditing"
+                @focusout="finishEditing">
         <button type="submit" class="add-button">追加</button>
     </form>
 </template>
@@ -13,12 +15,28 @@ export default{
     data: function() {
         return {
             title: '',
+            isEditing: false,
+        }
+    },
+    computed: {
+        classList() {
+            const classList = ['addlist']
+            if (this.isEditing) {
+                classList.push('active')
+            }
+            return classList
         }
     },
     methods: {
         addList: function() {
             this.$store.dispatch('addlist', {title: this.title})
             this.title = ''
+        },
+        startEditing: function() {
+            this.isEditing = true
+        },
+        finishEditing: function() {
+            this.isEditing = false
         },
     }
 }
