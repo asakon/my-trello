da8c60f8574f1b0f327bc63116b40dea3e12c4ea
Asako Kondo <kondoasako@gmail.com>
2021-12-21 18:41:55 +0900

    カードを保存する 等
    デフォルトのカードリスト等も設定

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,48 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const savedLists = localStorage.getItem('trello-lists')
+
+const store = new Vuex.Store({
   state: {
+    lists: savedLists ? JSON.parse(savedLists): [
+      {
+        title: 'Backlog',
+        cards: [
+          { body: '英語'},
+          { body: '数学'},
+        ]
+      },
+      {
+        title: 'Todo',
+        cards: [
+          {
+            body: '科学'
+          }
+        ]
+      },
+      {
+        title: 'Doing',
+        cards: []
+      }
+    ],
   },
   mutations: {
+    addlist(state, payload) {
+      state.lists.push({ title: payload.title, cards:[] })
+    },
   },
   actions: {
+    addlist(context, payload) {
+      context.commit('addlist', payload)
+    },
   },
-  modules: {
+  getters: {
   }
 })
+
+store.subscribe((mustation, state) => {
+  localStorage.setItem('trello-lists', JSON.stringify(state.lists))
+})
+
+export default store
