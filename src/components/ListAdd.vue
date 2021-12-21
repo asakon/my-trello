b05f61a5c6a3b22cb4986dc03bd7d33c7c5a7ab7
Asako Kondo <kondoasako@gmail.com>
2021-12-21 18:21:03 +0900

    フォームを追加

---
+++ src/components/ListAdd.vue
@@ -0,0 +1,25 @@
+<template>
+    <form class="addlist" @submit.prevent="addList">
+        <input v-model="title"
+                type="text"
+                class="text-input"
+                placeholder="リストを追加">
+        <button type="submit" class="add-button">追加</button>
+    </form>
+</template>
+
+<script>
+export default{
+    data: function() {
+        return {
+            title: '',
+        }
+    },
+    methods: {
+        addList: function() {
+            this.$store.dispatch('addlist', {title: this.title})
+            this.title = ''
+        },
+    }
+}
+</script>
